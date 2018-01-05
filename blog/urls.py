b49d55933537e1568587433f9907c41470a8c58e
ahsansamya <samyakahsan@gmail.com>
2018-01-04 21:45:54 -0600

    added views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    <a class="btn btn-default" href="{% url 'post_edit' pk=post.pk %}"><span class="glyphicon glyphicon-pencil"></span></a>
+
 ]
